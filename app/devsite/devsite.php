045e0a55d61d07c7c1cf30fdea2bb7488669a38b
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-07-05 10:47:25 -0400

    update gitignore

--- app/devsite/devsite.php
+++ app/devsite/devsite.php
@@ -5,20 +5,17 @@
 			<hr>
 		</div>
 	</div>
-	<div ng-repeat="category in courses" class="row">
-		<div class="col-sm-12">
-			<h2>{{ category.title }}</h2>
-			<br>
-		</div>
-		<div ng-repeat="course in category.courseList">
-			<div ng-if="course.status == filter">
-				<div class="col-md-6 hero-feature text-center">
-					<div class="thumbnail">
-						<div class="caption">
-							<h3>{{ course.code }}</h3>
-							<h5>{{ course.name }}</h5>
+	<div class="row">
+		<div ng-repeat="category in courses">
+			<div ng-repeat="course in category.courseList">
+				<div ng-if="course.status == filter">
+					<div class="col-md-3 hero-feature text-center">
+						<div class="thumbnail">
+							<div class="caption">
+								<h3>{{ course.code }}</h3>
+							</div>
+							<a href="#!/{{ type }}/{{ course.code }}" class="btn btn-primary btn-block">{{ action }} Course</a>
 						</div>
-						<a href="#!/{{ type }}/{{ course.code }}" class="btn btn-primary btn-block">{{ action }} Course</a>
 					</div>
 				</div>
 			</div>
